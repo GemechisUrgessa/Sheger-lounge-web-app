aa2c28403ea3d1e8d92dbef15cd41ae3f066d859
GemechisUrgessa <gemechisurgessa@gmail.com>
2023-01-20 04:15:13 +0300

    inital folder setup

--- server/app.js
+++ server/app.js
@@ -1,3 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import env from 'dotenv';
+import cors from 'cors';
 
+env.config();
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
